f9b762b200d1c85873a869849d9a17d9099de9fb
talat <talat@localhost.localdomain>
2014-06-22 18:55:25 +0530

    Insert into Database is Done

--- products/dbWrapper.py
+++ products/dbWrapper.py
@@ -17,5 +17,31 @@ from sqlalchemy import select
 class PreSet(object):
     """
     """
-    engine = create_engine('mysql+mysqldb://root:root@localhost/clinic')
+    engine = create_engine('mysql+mysqldb://root:root@localhost/creative')
     metadata = MetaData(engine)
+
+
+
+class DBAPI(object):
+
+    large_table = Table('large_table', PreSet.metadata,
+            Column('identity_idn', Integer, primary_key=True),
+            Column('user_idn', Integer),
+            Column('tag', String(20)),
+            Column('pool', Integer)
+            )
+
+    @classmethod
+    def create(cls, table_name=''):
+        import pdb; pdb.set_trace()
+        table_ref = cls.__getattribute__(cls, table_name)
+        if table_ref:
+            table_ref.create()
+
+    @classmethod
+    def insert(cls, _many='', **_single):
+        pass
+
+    @classmethod
+    def select(cls):
+        pass
