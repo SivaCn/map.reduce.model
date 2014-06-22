8b07bdfd13cf1c4b7c2668dbb114e04bc7fe517a
talat <talat@localhost.localdomain>
2014-06-22 16:23:31 +0530

    Tested with xmlrpc calls

--- run_http_server.py
+++ run_http_server.py
@@ -3,18 +3,23 @@
 #coding: utf-8
 
 import bottle
+from products import engine
 
 
 @bottle.route('/')
 @bottle.route('/index.html')
 def index():
+    import pdb; pdb.set_trace()
     return '<h1>Go to Hello World page</h1>'
 
 def main():
 
+    ## configs.
+    hostname = "0.0.0.0"
     # Start the Bottle webapp
     bottle.debug(True)
-    bottle.run(host='0.0.0.0', port=80, reloader=True)
+    engine.while_start(hostname=hostname, port=8585)
+    bottle.run(host=hostname, port=80, reloader=True)
 
 if __name__ == "__main__":
     main()
